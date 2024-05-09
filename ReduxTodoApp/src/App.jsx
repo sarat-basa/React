63e51328fda7772ccff032c64711c762f9ffe787
sarat <sarat.basa@gmail.com>
2024-05-10 00:33:03 +0530

    reduxtodoApp

---
+++ ReduxTodoApp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-gray-500 text-white h-10'>Redux Todo App</h1>
+     
+    </>
+  )
+}
+
+export default App
